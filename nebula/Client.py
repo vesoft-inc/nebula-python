6b67ba45677b13fcb3e4482b83afa767afb5805e
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-05-25 15:45:53 +0800

    Fix test failed (#25)
    * thread safety
    
    * add set space for reconnect
    
    * remove support python 2.7

--- nebula/Client.py
+++ nebula/Client.py
@@ -6,6 +6,7 @@
 # attached with Common Clause Condition 1.0, found in the LICENSES directory.
 
 
+import threading
 from .Common import *
 from graph.ttypes import ErrorCode
 from thrift.transport.TTransport import TTransportException
@@ -24,6 +25,8 @@ class GraphClient(object):
         self._retry_num = 3
         self._user = None
         self._password = None
+        self._space = None
+        self._lock = threading.Lock()
 
     def authenticate(self, user, password):
         """authenticate to graph server
@@ -37,23 +40,24 @@ class GraphClient(object):
                 - session_id
                 - error_msg
         """
-        if self._client is None:
-            raise AuthException("No client")
+        with self._lock:
+            if self._client is None:
+                raise AuthException("No client")
 
-        self._user = user
-        self._password = password
+            self._user = user
+            self._password = password
 
-        try:
-            resp = self._client.authenticate(user, password)
-            if resp.error_code:
+            try:
+                resp = self._client.authenticate(user, password)
+                if resp.error_code:
+                    return resp
+                else:
+                    self._is_ok = True
+                    self._session_id = resp.session_id
+                    print("client: %d authenticate succeed" % self._session_id)
                 return resp
-            else:
-                self._is_ok = True
-                self._session_id = resp.session_id
-                print("client: %d authenticate succeed" % self._session_id)
-            return resp
-        except Exception as x:
-            raise AuthException("Auth failed: {}".format(x))
+            except Exception as x:
+                raise AuthException("Auth failed: {}".format(x))
 
     def execute(self, statement):
         """execute statement to graph server
@@ -65,28 +69,28 @@ class GraphClient(object):
                 - error_code
                 - error_msg
         """
-        if self._client is None:
-            raise ExecutionException("No client")
+        with self._lock:
+            if self._client is None:
+                raise ExecutionException("No client")
 
-        try:
-            if not self._is_ok:
-                if not self.reconnect():
-                    raise ExecutionException("Execute `{}' failed: {}".format(statement,
-                                                                              'reconnect failed'))
-            resp = self._client.execute(self._session_id, statement)
-            retry_num = self._retry_num;
-            if resp.error_code != ErrorCode.SUCCEEDED:
-                while retry_num > 0:
-                    retry_num -= 1
-                    resp = self._client.execute(self._session_id, statement)
-                    if resp.error_code == ErrorCode.SUCCEEDED:
-                        return SimpleResponse(resp.error_code, resp.error_msg)
-            return SimpleResponse(resp.error_code, resp.error_msg)
-        except TTransportException as x:
-            self._is_ok = False
-            raise ExecutionException("Execute `{}' failed: {}".format(statement, x))
-        except Exception as x:
-            raise ExecutionException("Execute `{}' failed: {}".format(statement, x))
+            try:
+                if not self._is_ok:
+                    if not self.reconnect():
+                        raise ExecutionException("Execute `{}' failed: {}".format(statement, 'reconnect failed'))
+                resp = self._client.execute(self._session_id, statement)
+                retry_num = self._retry_num;
+                if resp.error_code != ErrorCode.SUCCEEDED:
+                    while retry_num > 0:
+                        retry_num -= 1
+                        resp = self._client.execute(self._session_id, statement)
+                        if resp.error_code == ErrorCode.SUCCEEDED:
+                            return SimpleResponse(resp.error_code, resp.error_msg)
+                return SimpleResponse(resp.error_code, resp.error_msg)
+            except TTransportException as x:
+                self._is_ok = False
+                raise ExecutionException("Execute `{}' failed: {}".format(statement, x))
+            except Exception as x:
+                raise ExecutionException("Execute `{}' failed: {}".format(statement, x))
 
     def execute_query(self, statement):
         """execute query statement to graph server
@@ -102,27 +106,32 @@ class GraphClient(object):
                 - rows
                 - space_name
         """
-        if self._client is None:
-            raise ExecutionException("No client")
+        with self._lock:
+            if self._client is None:
+                raise ExecutionException("No client")
 
-        try:
-            return self._client.execute(self._session_id, statement)
-        except Exception as x:
-            raise ExecutionException("Execute `{}' failed: {}".format(statement, x))
+            try:
+                return self._client.execute(self._session_id, statement)
+            except TTransportException as x:
+                self._is_ok = False
+                raise ExecutionException("Execute `{}' failed: {}".format(statement, x))
+            except Exception as x:
+                raise ExecutionException("Execute `{}' failed: {}".format(statement, x))
 
     def sign_out(self):
         """sign out: Users should call sign_out when catch the exception or exit
         """
-        if self._client is None:
-            return
+        with self._lock:
+            if self._client is None:
+                return
 
-        try:
-            if self._session_id is not None:
-                print('client: %d sign out' % self._session_id)
-            self._client.signout(self._session_id)
-            self._pool.return_connection(self._client)
-        except Exception as x:
-            raise Exception('SignOut failed: {}'.format(x))
+            try:
+                if self._session_id is not None:
+                    print('client: %d sign out' % self._session_id)
+                self._client.signout(self._session_id)
+                self._pool.return_connection(self._client)
+            except Exception as x:
+                raise Exception('SignOut failed: {}'.format(x))
 
     def reconnect(self):
         """reconnect: reconnect the server
@@ -130,6 +139,7 @@ class GraphClient(object):
             True or False
         """
         try:
+            self._client._iprot.trans.close()
             self._client._iprot.trans.open()
             if self._user is None or self._password is None:
                 self._is_ok = True
@@ -140,8 +150,26 @@ class GraphClient(object):
             else:
                 self._session_id = resp.session_id
                 print("client: %d authenticate succeed" % self._session_id)
+            if self._space is None:
+                self._is_ok = True
+                return True
+            resp = self._client.execute(self._session_id, 'USE {}'.format(self._space));
+            if resp.error_code != ErrorCode.SUCCEEDED:
+                return False
             self._is_ok = True
             return True
         except Exception as x:
             print(x)
             return False
+
+    def set_space(self, space):
+        """set_space: set the space when reconnect need it
+        """
+        self._space = space
+
+    def is_none(self):
+        """is_none: determine if the client creation was successful
+        Returns:
+            True or False
+        """
+        return self._client is None
