e2e2eb814cd54ffcf888263831c94b372e45dde5
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-07-24 18:40:13 +0800

    add example to nebula2

--- setup2.py
+++ setup2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2019 vesoft inc. All rights reserved.
+# Copyright (c) 2020 vesoft inc. All rights reserved.
 #
 # This source code is licensed under Apache 2.0 License,
 # attached with Common Clause Condition 1.0, found in the LICENSES directory.
@@ -12,7 +12,7 @@ setup(
     name='nebula2-python',
     version="0.0.1-4",
     license = "Apache 2.0 + Common Clause 1.0",
-    description='Python client for Nebula Graph',
+    description='Python client for Nebula Graph 2.0',
     url='https://github.com/vesoft-inc/nebula-python',
     author='darion.wang',
     author_email='darion.wang@vesoft-inc.com',
