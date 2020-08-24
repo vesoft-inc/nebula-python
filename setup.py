0ce867704c86e7ea1c1fb7ec490e213a2b88a676
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-08-24 13:53:51 +0800

    update readme

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nebula2-python',
-    version="0.0.1-7",
+    version="0.0.1-8",
     license = "Apache 2.0 + Common Clause 1.0",
     description='Python client for Nebula Graph 2.0',
     url='https://github.com/vesoft-inc/nebula-python',
