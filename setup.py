e9bf985fbfa27410946849158e0ad92556eff722
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-08-24 10:45:01 +0800

    fix loss future (#37)

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='nebula2-python',
-    version="0.0.1-6",
+    version="0.0.1-7",
     license = "Apache 2.0 + Common Clause 1.0",
     description='Python client for Nebula Graph 2.0',
     url='https://github.com/vesoft-inc/nebula-python',
     author='darion.wang',
     author_email='darion.wang@vesoft-inc.com',
     install_requires=['httplib2',
-                      'future; python_version == "2.7"',
+                      'future',
                       'six',
                       'futures; python_version == "2.7"'],
     packages=find_packages(),
