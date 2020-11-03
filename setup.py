b73ad27a1f1e64560ddadc9f74a30672980176c8
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-11-03 21:17:36 +0800

    modify doc

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nebula-python',
-    version="1.1.0",
+    version="1.1.1",
     license = "Apache 2.0 + Common Clause 1.0",
     description='Python client for Nebula Graph',
     url='https://github.com/vesoft-inc/nebula-python',
