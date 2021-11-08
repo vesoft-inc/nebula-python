4df4c5709d14217af4e9fa17ab73280ef61e1e6f
Harris.Chu <1726587+HarrisChu@users.noreply.github.com>
2021-11-08 13:43:21 +0800

    Remove Common Clause License (#167)

--- nebula2/__init__.py
+++ nebula2/__init__.py
@@ -3,8 +3,8 @@
 
 # Copyright (c) 2020 vesoft inc. All rights reserved.
 #
-# This source code is licensed under Apache 2.0 License,
-# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+# This source code is licensed under Apache 2.0 License.
+
 
 from nebula2.common.ttypes import Value
 Value.__hash__ = lambda self: self.value.__hash__()
