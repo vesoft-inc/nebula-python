96221372c817de830775453101d8e982cd70fc85
Aiee <18348405+Aiee@users.noreply.github.com>
2021-12-21 11:22:52 +0800

    Tune action

--- tests/test_session.py
+++ tests/test_session.py
@@ -50,8 +50,8 @@ class TestSession(TestCase):
             )
             for i in range(0, 5):
                 if i == 3:
-                    os.system('docker stop nebula-docker-compose_graphd0_1')
-                    os.system('docker stop nebula-docker-compose_graphd1_1')
+                    os.system('docker stop tests_graphd0_1')
+                    os.system('docker stop tests_graphd1_1')
                     time.sleep(3)
                 # the session update later, the expect test
                 # resp = session.execute('SHOW TAGS')
@@ -65,8 +65,8 @@ class TestSession(TestCase):
         except Exception as e:
             assert False, e
         finally:
-            os.system('docker start nebula-docker-compose_graphd0_1')
-            os.system('docker start nebula-docker-compose_graphd1_1')
+            os.system('docker start tests_graphd0_1')
+            os.system('docker start tests_graphd1_1')
             time.sleep(5)
 
     def test_3_session_context(self):
