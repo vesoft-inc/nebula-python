53d8aae3ad7b42e19320b5f2cc2792a0f5093953
Aiee <18348405+Aiee@users.noreply.github.com>
2022-04-25 17:25:04 +0800

    Fix deploy action

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (base_dir / 'README.md').read_text()
 
 setup(
     name='nebula3-python',
-    version='3.0.0',
+    version='3.1.0',
     license='Apache 2.0',
     author='vesoft-inc',
     author_email='info@vesoft.com',
