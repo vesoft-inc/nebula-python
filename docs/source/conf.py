3e0d9ec317ea66d8fc24108b41a16360b1ce5394
Anqi <anqi.wang@vesoft.com>
2023-09-19 15:11:50 +0800

    add sphinx conf for auto generate api doc

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'nebula-python-doc'
+copyright = '2023, Nicole'
+author = 'Nicole'
+release = 'v3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import os
+import sys
+sys.path.insert(0,os.path.abspath('../../'))
+
+extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
