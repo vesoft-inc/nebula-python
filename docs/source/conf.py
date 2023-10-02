72f22552e9d3f1d6d246d56ae85e23b20555be52
liuxiao <liuxiao2103@qq.com>
2023-10-02 14:10:20 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append(os.path.abspath(
-    os.path.join(__file__, '../../../nebula3')
+    os.path.join(__file__, '../../../nebula3/gclient')
 ))
 
 # -- Project information -----------------------------------------------------
