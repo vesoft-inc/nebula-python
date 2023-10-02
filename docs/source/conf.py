2db64be9e136247a5128df06c52373e85757fcc8
liuxiao <liuxiao2103@qq.com>
2023-10-02 14:49:55 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.append(os.path.abspath(
-    os.path.join(__file__, '../../../nebula3/gclient')
-))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
