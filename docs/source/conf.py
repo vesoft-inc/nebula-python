44a1c7a77118d9a4deec331e892db705c55d4b9e
liuxiao <liuxiao2103@qq.com>
2023-10-02 14:02:26 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../nebula3'))
+# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '../..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
