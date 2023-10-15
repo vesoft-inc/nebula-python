244da0780d16db918373ff323a9a25dfed622e05
liuxiao <liuxiao2103@qq.com>
2023-10-15 21:01:51 +0800

    exclude

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,12 @@
 pip install --user sphinx furo
-sphinx-apidoc -o source ../nebula3
+sphinx-apidoc -f -o source ../nebula3 \
+      ../nebula3/common/* \
+      ../nebula3/data/* \
+      ../nebula3/fbthrift/* \
+      ../nebula3/graph/* \
+      ../nebula3/mclient/* \
+      ../nebula3/meta/* \
+      ../nebula3/sclient/* \
+      ../nebula3/storage/*
 make clean
 make html
--- docs/source/nebula3.rst
+++ docs/source/nebula3.rst
@@ -7,15 +7,7 @@ Subpackages
 .. toctree::
    :maxdepth: 4
 
-   nebula3.common
-   nebula3.data
-   nebula3.fbthrift
    nebula3.gclient
-   nebula3.graph
-   nebula3.mclient
-   nebula3.meta
-   nebula3.sclient
-   nebula3.storage
 
 Submodules
 ----------
