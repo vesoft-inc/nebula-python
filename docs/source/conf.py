baca6f1631f6e891f0e28ca2866cf3f5a2ae8527
Wey Gu <weyl.gu@gmail.com>
2023-10-23 16:45:37 +0800

    chore: read the doc slug update (#295)
    * chore: read the doc slug update
    
    ref: #292
    
    * chore: wording of the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'nebula-python-doc'
+project = 'NebulaGraph Python'
 copyright = '2023, Nicole'
 author = 'Nicole'
 release = 'v3'
