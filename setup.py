8863a2b2f5aadb546581bbb7b88ec84b1be45046
Wey Gu <weyl.gu@gmail.com>
2024-04-25 11:29:29 +0800

    fix: missing dep in python3.6
    httpcore-->anyio-->contextvars when it's < python3.7
    Whoile this was not properly handled by setup.py
    
    Thus this failed, somehow it rollback to .local cache
    in ci runner.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'six >= 1.16.0',
         'pytz >= 2021.1',
         'httpx[http2] >= 0.22.0',
+        'contextvars==2.4', # for python 3.6
     ],
     packages=find_packages(),
     platforms=['3.6, 3.7, 3.8, 3.9, 3.10, 3.11, 3.12'],
